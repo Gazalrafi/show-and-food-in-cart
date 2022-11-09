1eb09ba616a83ab431b0e81b0ed01704e43123ad
Gazalrafi <grafi7485>
2022-11-09 17:49:48 +0530

    creting form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,6 +1,10 @@
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
+
 const MealItem=(props)=>{
-    const price=`$${props.price.toFixed(2)}`; // toFixed for decimal places
+
+const price=`$${props.price.toFixed(2)}`; // toFixed for decimal places
+
 return <li className={classes.meal}>
     <div>
         <h3>{props.name}</h3>
@@ -8,7 +12,7 @@ return <li className={classes.meal}>
     <div className={classes.description}>{props.description}</div>
     <div className={classes.price}>{price}</div>
     <div>
-
+ <MealItemForm/>
     </div>
 </li>
 }
