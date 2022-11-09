00c5df2c7eadce9113df1e6ecb2409d3c910879c
Gazalrafi <grafi7485>
2022-11-09 22:46:47 +0530

    making cart

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import Header from'./components/Layout/Header';
 import {Fragment} from 'react';
 import Meals from './components/Meals/Meals.js';
+import Cart from './components/Cart/Cart.js';
 
 function App() {
   return (
     <Fragment>
+      <Cart/>
       <Header/>
       <main>
         <Meals/>
