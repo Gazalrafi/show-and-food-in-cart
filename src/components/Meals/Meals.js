0d7a5cad1d9d3497bf167d6c8d32db3f26824983
Gazalrafi <grafi7485>
2022-11-09 12:43:43 +0530

    items visible

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,8 +1,10 @@
 import MealsSummary from './MealsSummary.js';
+import AvailableMeals from './AvailableMeals.js';
 import {Fragment} from 'react';
 const Meals =()=>{
 return <Fragment>
     <MealsSummary/>
+    <AvailableMeals/>
 </Fragment>
 };
 export default Meals;
